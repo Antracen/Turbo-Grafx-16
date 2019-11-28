cb64ea18b305e49526c963c16334bff9a8a2094f
wass <wass@W530.Greenvalley>
2019-11-28 14:17:30 +0100

    Assignment 4

--- First_OpenACC/matrix.c
+++ First_OpenACC/matrix.c
@@ -1,4 +1,6 @@
-// pgcc -acc -ta=tesla:cc3x,nvidia,lineinfo -Minfo=accel matrix.cpp -o code
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 /*
     Calculate C = A*B matrix multiplication
@@ -6,7 +8,7 @@
 void matmul_acc(float *C, const float *A, const float *B, int A_rows, int A_cols, int B_rows, int B_cols) {
 
     if(A_cols != B_rows) {
-        return A_cols/0; // throw error
+        int error = A_cols/0; // throw error
     }
 
     int C_rows = A_rows;
@@ -15,7 +17,7 @@ void matmul_acc(float *C, const float *A, const float *B, int A_rows, int A_cols
     #pragma acc parallel loop copyin(A[0:A_rows*A_cols]) copyin(B[0:B_rows*B_cols]) copyout(C[0:C_rows*C_cols])
     for(int i = 0; i < C_rows; i++) {
         #pragma acc loop
-        for(int j = 0; j < C_cols, j++) {
+        for(int j = 0; j < C_cols; j++) {
             float sum = 0.0;
             for(int k = 0; k < A_cols; k++) {
                 sum += A[i*A_rows + k]*B[k*B_rows + j];
@@ -31,14 +33,14 @@ void matmul_acc(float *C, const float *A, const float *B, int A_rows, int A_cols
 void matmul(float *C, const float *A, const float *B, int A_rows, int A_cols, int B_rows, int B_cols) {
 
     if(A_cols != B_rows) {
-        return A_cols/0; // throw error
+        int error = A_cols/0; // throw error
     }
 
     int C_rows = A_rows;
     int C_cols = B_cols;
 
     for(int i = 0; i < C_rows; i++) {
-        for(int j = 0; j < C_cols, j++) {
+        for(int j = 0; j < C_cols; j++) {
             float sum = 0.0;
             for(int k = 0; k < A_cols; k++) {
                 sum += A[i*A_rows + k]*B[k*B_rows + j];
@@ -49,5 +51,32 @@ void matmul(float *C, const float *A, const float *B, int A_rows, int A_cols, in
 }
 
 int main() {
+    int N = 1500;
 
+    float *A = (float*) malloc(sizeof(float)*N*N);
+    float *B = (float*) malloc(sizeof(float)*N*N);
+    float *C = (float*) malloc(sizeof(float)*N*N);
+
+    for(int i = 0; i < N*N; i++) {
+        A[i] = 1000*((float) rand() / RAND_MAX);
+        B[i] = 1000*((float) rand() / RAND_MAX);
+        C[i] = 0.0;
+    }
+
+    clock_t start, end;
+    double exec_time;
+
+    start = clock();
+    matmul(C, A, B, N, N, N, N);
+    end = clock();
+    exec_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("CPU: %f\n", exec_time);
+
+    start = clock();
+    matmul_acc(C, A, B, N, N, N, N);
+    end = clock();
+    exec_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("GPU: %f\n", exec_time);
+
+    return 0;
 }
